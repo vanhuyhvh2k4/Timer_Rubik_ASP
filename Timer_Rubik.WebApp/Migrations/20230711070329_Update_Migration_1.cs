c3039f4c448c5afba0633fdf4ad8635e45c381b1
anhhuydepzai <anhhuydepzai@gmail.com>
2023-07-11 14:10:47 +0700

    fixbug/update favorite model

---
+++ Timer_Rubik.WebApp/Migrations/20230711070329_Update_Migration_1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Timer_Rubik.WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Migration_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Time",
+                table: "Favorites",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Time",
+                table: "Favorites",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "float");
+        }
+    }
+}
