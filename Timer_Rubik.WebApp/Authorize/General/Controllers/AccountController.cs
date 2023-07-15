c51adff02465f951fd6d898b1718e1627c75e103
anhhuydepzai <anhhuydepzai@gmail.com>
2023-07-15 21:38:33 +0700

    feature/login method for accountController

--- Timer_Rubik.WebApp/Authorize/General/Controllers/AccountController.cs
+++ Timer_Rubik.WebApp/Authorize/General/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Timer_Rubik.WebApp.Authorize.General.DTO;
+using Timer_Rubik.WebApp.Interfaces;
+using Timer_Rubik.WebApp.Utils;
 
 namespace Timer_Rubik.WebApp.Authorize.General.Controllers
 {
@@ -7,12 +9,53 @@ namespace Timer_Rubik.WebApp.Authorize.General.Controllers
     [Route("api/account")]
     public class AccountController : Controller
     {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public IActionResult Login([FromBody] LoginRequest loginRequest)
         {
             try
             {
-                return Ok();
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var accountEntity = _accountService.GetAccount(loginRequest.Email.Trim());
+
+                if (accountEntity == null)
+                {
+                    return NotFound("Not Found Account");
+                }
+
+                bool isCorrectPassword = Password.VerifyPassword(loginRequest.Password.Trim(), accountEntity.Password.Trim());
+
+                if (!isCorrectPassword)
+                {
+                    return StatusCode(403, "Password is not correct");
+                }
+
+                var accessToken = JWT.GenerateAccessToken(accountEntity.Id.ToString(), accountEntity.RuleId.ToString());
+
+                var refreshToken = JWT.GenerateRefreshToken(accountEntity.Id.ToString(), accountEntity.RuleId.ToString());
+
+                var response = new
+                {
+                    accessToken = accessToken,
+                    refreshToken = refreshToken,
+                };
+
+                return Ok(response);
             } catch (Exception ex)
             {
                 return StatusCode(500, new
---
+++ Timer_Rubik.WebApp/Utils/JWT.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Timer_Rubik.WebApp.Utils
+{
+    public class JWT
+    {
+        public static string GenerateAccessToken(string userId, string ruleId)
+        {
+            var claims = new[]
+            {
+                new Claim("userId", userId),
+                new Claim("RuleId", ruleId),
+            };
+
+            // Create the token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("timer-rubik-secret-access-token");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddSeconds(30),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                                                            SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // Return the token as a string
+            return tokenHandler.WriteToken(token);
+        }
+
+        public static string GenerateRefreshToken(string userId, string ruleId)
+        {
+            var claims = new[]
+            {
+                new Claim("userId", userId),
+                new Claim("RuleId", ruleId),
+            };
+
+            // Create the token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("timer-rubik-secret-refresh-token");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(365),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                                                            SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // Return the token as a string
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
