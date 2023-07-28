ade05e2a675eb0d3a80adaeec53157d51f665b2f
anhhuydepzai <anhhuydepzai@gmail.com>
2023-07-28 22:25:58 +0700

    feature/config razor page

--- Timer_Rubik.WebApp/Program.cs
+++ Timer_Rubik.WebApp/Program.cs
@@ -17,6 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -82,6 +83,7 @@ app.UseMiddleware<AdminTokenMiddleware>();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
+    endpoints.MapRazorPages();
 });
 
 app.Run();
